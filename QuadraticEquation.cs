a9e138a21dcde25384f45aefa3378627d5c89709
dseregin <dseregin@mail.ru>
2017-03-22 20:21:42 +0300

    Реализация класса для решения квадратного уравнения

---
+++ QuadraticEquation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Решение_квадратного_уравнения
+{
+    public class QuadraticEquation
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public double X1 { get; private set; }
+        public double X2 { get; private set; }
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public bool Solve()
+        {
+            double D = Math.Pow(B, 2) - 4 * A * C;
+
+            if (D < 0)
+            {
+                Console.WriteLine("Действительных корней нет");
+                
+                return false;
+            }
+            else
+            {
+                X1 = (-B + Math.Sqrt(D)) / (2 * A);
+                X2 = (-B - Math.Sqrt(D)) / (2 * A);
+                
+                return true;
+            }
+        }
+    }
+}
